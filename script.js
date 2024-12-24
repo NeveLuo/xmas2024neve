479d810c1764f0110999b1170c26c717c5eb05a8
NeveLuo <loitien@163.com>
2024-12-24 11:51:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,29 @@ let snowflakesStopped = false;
 let musicSwitched = false;
 let imagesLoaded = false;
 let userInput = "";
+let backgroundMusic, newMusic;
+let image1, image2;
 let buttonX, buttonY, buttonWidth, buttonHeight;
 
-// 图片对象
-let image1, image2;
-let backgroundMusic, newMusic;
-
 function preload() {
-  backgroundMusic = loadSound("background_music.mp3");
-  image1 = loadImage("image1.png");
-  image2 = loadImage("image2.png");
+  backgroundMusic = loadSound('./assets/background_music.mp3');
+  newMusic = loadSound('./assets/new_music.mp3');
+  image1 = loadImage('./assets/image1.png');
+  image2 = loadImage('./assets/image2.png');
 }
 
 function setup() {
   createCanvas(1200, 800);
+  noStroke();
   frameRate(38);
 
-  // 初始化雪花
   for (let i = 0; i < 10; i++) {
     snowflakes.push(new PixelSnowflake(random(width), random(height)));
   }
 
-  // 播放背景音乐
-  backgroundMusic.setVolume(0.4);
+  backgroundMusic.setVolume(0.6);
   backgroundMusic.loop();
 
-  // 初始化按钮位置和尺寸
   buttonWidth = 72;
   buttonHeight = 35;
   buttonX = width / 2 - buttonWidth / 2;
@@ -39,7 +36,6 @@ function setup() {
 function draw() {
   drawGradientBackground();
 
-  // 更新和绘制雪花
   for (let i = snowflakes.length - 1; i >= 0; i--) {
     let s = snowflakes[i];
     s.update();
@@ -49,19 +45,16 @@ function draw() {
     }
   }
 
-  // 控制雪花生成
   if (!snowflakesStopped && frameCount % 7 === 0) {
     snowflakes.push(new PixelSnowflake(random(width), random(height / 2)));
   }
 
-  // 显示弹窗
   if (showPopup) {
     drawPopup();
     drawButton();
     drawInputField();
   }
 
-  // 显示加载的图片
   if (imagesLoaded) {
     drawImages();
     drawImagePopup();
@@ -83,14 +76,22 @@ function mousePressed() {
     if (!musicSwitched) {
       musicSwitched = true;
       backgroundMusic.stop();
-      newMusic = loadSound("new_music.mp3", () => newMusic.play());
+      newMusic.play();
     }
 
     imagesLoaded = true;
   }
 }
 
-// 绘制渐变背景
+function drawImages() {
+  if (image1) {
+    image(image1, 600, 400, 480, 360);
+  }
+  if (image2) {
+    image(image2, 80, 400, 480, 360);
+  }
+}
+
 function drawGradientBackground() {
   for (let y = 0; y < height; y++) {
     let gradient = map(y, 0, height, 0, 50);
@@ -99,7 +100,6 @@ function drawGradientBackground() {
   }
 }
 
-// 绘制弹窗
 function drawPopup() {
   fill(40, 40, 40, 200);
   rect(width / 2 - 175, height / 2 - 102, 350, 204, 20);
@@ -107,10 +107,9 @@ function drawPopup() {
   fill(255);
   textSize(18);
   textAlign(CENTER, CENTER);
-  text("Enter your name here\n(English only)", width / 2, height / 2 - 60);
+  text('Enter your name here \n(English only)', width / 2, height / 2 - 60);
 }
 
-// 绘制按钮
 function drawButton() {
   fill(100, 100, 250);
   rect(buttonX, buttonY, buttonWidth, buttonHeight, 10);
@@ -118,10 +117,9 @@ function drawButton() {
   fill(255);
   textSize(18);
   textAlign(CENTER, CENTER);
-  text("OK", buttonX + buttonWidth / 2, buttonY + buttonHeight / 2);
+  text('OK', buttonX + buttonWidth / 2, buttonY + buttonHeight / 2);
 }
 
-// 绘制输入框
 function drawInputField() {
   fill(255, 255, 255, 200);
   rect(width / 2 - 128, height / 2 - 23, 256, 45, 5);
@@ -132,25 +130,19 @@ function drawInputField() {
   text(userInput, width / 2, height / 2);
 }
 
-// 绘制图片
-function drawImages() {
-  if (image1) {
-    image(image1, 600, 400, 480, 360);
-  }
-  if (image2) {
-    image(image2, 80, 400, 480, 360);
-  }
-}
-
-// 绘制图片弹窗
 function drawImagePopup() {
   fill(40, 40, 40, 180);
   rect(width / 2 - 275, height / 2 - 300, 550, 550, 20);
 
   let displayText =
-    "Dear " +
+    'Dear ' +
     userInput +
-    ":\n\nMerry Christmas!\nMay you love and be loved.\n\nYours,\nNeve\n24 Dec 24";
+    ':\n\n\n\n' +
+    'Merry Christmas!\n\n' +
+    'May you love and be loved.\n\n\n\n' +
+    'Yours,\n\n' +
+    'Neve\n\n' +
+    '24 Dec 24';
 
   fill(255);
   textSize(21);
@@ -158,7 +150,6 @@ function drawImagePopup() {
   text(displayText, width / 2, height / 2 - 70);
 }
 
-// 定义雪花类
 class PixelSnowflake {
   constructor(x, y) {
     this.x = x;
@@ -180,6 +171,10 @@ class PixelSnowflake {
     }
   }
 
+  isFinished() {
+    return this.opacity <= 0;
+  }
+
   drawHexagon(x, y, radius) {
     beginShape();
     for (let i = 0; i < 6; i++) {
@@ -187,9 +182,5 @@ class PixelSnowflake {
       vertex(x + cos(angle) * radius, y + sin(angle) * radius);
     }
     endShape(CLOSE);
-  }
-
-  isFinished() {
-    return this.opacity <= 0;
   }
 }
